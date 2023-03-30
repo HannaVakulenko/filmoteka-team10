fff1cf62d8da6099df2621bee622d70216863683
Aleksandr-Kovshar <aleksandr.mpro@gmail.com>
2023-03-30 22:24:28 +0300

    add api and export api

--- src/index.js
+++ src/index.js
@@ -5,4 +5,25 @@ import './js/pagination';
 import './js/modal-film';
 import './js/modal-students';
 
-//import { refs } from './refs';
+// //import { refs } from './refs';
+
+// Імпорт Api:
+// змінні для GET запитів:
+import {
+  API_KEY,
+  API_UR,
+  MEDIA_TYPE,
+  TIME_WINDOW,
+  PER_PAGE,
+  page,
+  pages,
+} from './js/api-servise';
+// отриманні змінні:  запит клієнта (пошук), id фільму, масив попопулярних фільмів, масив знайдених фільмів
+import {
+  query,
+  my_movie_id,
+  filmsTrending,
+  searchFilms,
+} from './js/api-servise';
+// імпорт функцій: популярні фільми за тиждень, знайти фільми по ключовим словам, знайти фільм по ID
+import { FetchTrending, FetchSearch, FetchFilmID } from './js/api-servise';
