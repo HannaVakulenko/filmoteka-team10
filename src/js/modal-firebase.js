b189868d14354df083df579e6a34a74976eb30af
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-03-31 02:52:19 +0300

    Unpated classes (BEM)

--- src/js/modal-firebase.js
+++ src/js/modal-firebase.js
@@ -1,12 +1,13 @@
 const refs = {
     modal: document.querySelector(".modal"),
-    modalHeader: document.querySelector(".modal-title"),
-    modalForm: document.querySelector(".modal-form"),
-    registerText: document.querySelector(".modal-form__text-register"),
+    modalHeader: document.querySelector(".modal__title"),
+    modalForm: document.querySelector(".modal__form"),
+    registerText: document.querySelector(".modal__form-text-register"),
+    loginButton: document.querySelector(".modal__form-button--send"),
     registerButton: document.getElementById("register-btn"),
     openModalBtn: document.getElementById("open-modal-btn"),
     closeModalBtn: document.getElementById("close-modal-btn"),
-    loginButton: document.querySelector(".modal-button__send"),
+    
 }
 
 const initialModalHeaderText = refs.modalHeader.textContent;
@@ -38,48 +39,25 @@ closeModal();
 }
 });
 
-
-// function register(e) {
-//   e.preventDefault();
-//   let inputField;
-
-//   if (refs.modalHeader.textContent !== "Register") {
-//     refs.modalHeader.textContent = "Register";
-//     refs.registerText.textContent = "I have an account";
-//     refs.registerButton.textContent = "Auth";
-//     refs.loginButton.textContent = "Register";
-
-//     registerRender();
-//   } else {
-//     refs.modalHeader.textContent = initialModalHeaderText;
-//     refs.registerText.textContent = initialRegisterText;
-//     refs.registerButton.textContent = "Register";
-//     refs.loginButton.textContent = initialRegisterButton;
-//     inputField = document.getElementById("input-name");
-//     inputField.parentNode.removeChild(inputField);
-//   }
-// }
 function register(e) {
-    e.preventDefault();
-  
-    const isRegistering = refs.modalHeader.textContent !== "Register";
-  
-    refs.modalHeader.textContent = isRegistering ? "Register" : initialModalHeaderText;
-    refs.registerText.textContent = isRegistering ? "I have an account" : initialRegisterText;
-    refs.registerButton.textContent = isRegistering ? "Auth" : "Register";
-    refs.loginButton.textContent = isRegistering ? "Register" : initialRegisterButton;
-  
-    if (isRegistering) {
-      registerRender();
-    } else {
-      const inputField = document.getElementById("input-name");
-      inputField?.parentNode.removeChild(inputField);
-    }
+  e.preventDefault();
+
+  const isRegistering = refs.modalHeader.textContent !== "Register";
+
+  refs.modalHeader.textContent = isRegistering ? "Register" : initialModalHeaderText;
+  refs.registerText.textContent = isRegistering ? "I have an account" : initialRegisterText;
+  refs.registerButton.textContent = isRegistering ? "Auth" : "Register";
+  refs.loginButton.textContent = isRegistering ? "Register" : initialRegisterButton;
+
+  if (isRegistering) {
+    registerRender();
+  } else {
+    const inputField = document.getElementById("input-name");
+    inputField?.parentNode.removeChild(inputField);
   }
+}
 
-
-  
   function registerRender(){
-    const inputHtml = '<input class="modal-form__input" type="text" placeholder="Name*" id="input-name" minlength="4" maxlength="25">';
+    const inputHtml = '<input class="modal__form-input" type="text" placeholder="Name*" id="input-name" minlength="4" maxlength="25">';
     refs.modalForm.insertAdjacentHTML("afterbegin", inputHtml);
   }
