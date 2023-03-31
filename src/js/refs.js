33dc05cf03040e652b8030f5084be932dd6efa06
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-03-31 13:20:39 +0300

    import refs

---
+++ src/js/refs.js
@@ -0,0 +1,13 @@
+const refs = {
+    modal: document.querySelector(".modal"),
+    modalHeader: document.querySelector(".modal__title"),
+    modalForm: document.querySelector(".modal__form"),
+    registerText: document.querySelector(".modal__form-text-register"),
+    loginButton: document.querySelector(".modal__form-button--send"),
+    registerButton: document.getElementById("register-btn"),
+    openModalBtn: document.getElementById("open-modal-btn"),
+    closeModalBtn: document.getElementById("close-modal-btn"),
+    inputFields: document.querySelectorAll(".modal__form-input"),
+    
+}
+export { refs }
