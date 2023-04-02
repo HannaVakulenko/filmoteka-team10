d877469657f85beae738efec82fdbae489597207
Swarp <m.kleban@terawatt-group.com>
2023-04-03 00:13:48 +0300

    add Other in to genres

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,67 +1,67 @@
-import {refs} from './refs'
-import 'animate.css';
+// import {refs} from './refs'
+// import 'animate.css';
 
-const img = new Image()
-img.src = 'https://media.tenor.com/ycXJZJnD2SwAAAAd/welcome-welcome-message-for-the-group.gif'
-let i = 0
+// const img = new Image()
+// img.src = 'https://media.tenor.com/ycXJZJnD2SwAAAAd/welcome-welcome-message-for-the-group.gif'
+// let i = 0
 
-window.addEventListener('beforeunload', () => {
-    if (i === 0) {
-        localStorage.removeItem('preloaderPlayed');
-    } else {
-        return
-    }
-})
+// window.addEventListener('beforeunload', () => {
+//     if (i === 0) {
+//         localStorage.removeItem('preloaderPlayed');
+//     } else {
+//         return
+//     }
+// })
 
 
-const linkOnSecondPageLogo = document.querySelector('.nav__logo')
+// const linkOnSecondPageLogo = document.querySelector('.nav__logo')
 
-let linkOnSecondPageHome
+// let linkOnSecondPageHome
 
-if (document.querySelector('.nav__li[href="/index.html"]')) {
-    // console.log(Boolean(document.querySelector('.nav__li[href="/index.html"]')))
-    linkOnSecondPageHome = document.querySelector('.nav__li[href="/index.html"]')
-}
-if (document.querySelector('.nav__li[href="/filmoteka-team10/index.html"]')) {
-    // console.log(Boolean(document.querySelector('.nav__li[href="/filmoteka-team10/index.html]"')))
-    linkOnSecondPageHome = document.querySelector('.nav__li[href="/filmoteka-team10/index.html"]')
-}
+// if (document.querySelector('.nav__li[href="/index.html"]')) {
+//     // console.log(Boolean(document.querySelector('.nav__li[href="/index.html"]')))
+//     linkOnSecondPageHome = document.querySelector('.nav__li[href="/index.html"]')
+// }
+// if (document.querySelector('.nav__li[href="/filmoteka-team10/index.html"]')) {
+//     // console.log(Boolean(document.querySelector('.nav__li[href="/filmoteka-team10/index.html]"')))
+//     linkOnSecondPageHome = document.querySelector('.nav__li[href="/filmoteka-team10/index.html"]')
+// }
 
-// console.log(linkOnSecondPageHome)
-const linkOnSecondPageLibrary = document.querySelector('[href="/my-library.html"]')
-let search
-let searchTest = document.querySelectorAll('.search-form')
-if (searchTest.length > 0){
-    search = document.querySelector('.search-form')
-    search.addEventListener('submit', onSecondPage)
-}
+// // console.log(linkOnSecondPageHome)
+// const linkOnSecondPageLibrary = document.querySelector('[href="/my-library.html"]')
+// let search
+// let searchTest = document.querySelectorAll('.search-form')
+// if (searchTest.length > 0){
+//     search = document.querySelector('.search-form')
+//     search.addEventListener('submit', onSecondPage)
+// }
 
-linkOnSecondPageLogo.addEventListener('click', onSecondPage);
-linkOnSecondPageHome.addEventListener('click', onSecondPage);
-linkOnSecondPageLibrary.addEventListener('click', onSecondPage);
+// linkOnSecondPageLogo.addEventListener('click', onSecondPage);
+// linkOnSecondPageHome.addEventListener('click', onSecondPage);
+// linkOnSecondPageLibrary.addEventListener('click', onSecondPage);
  
-if (!localStorage.getItem('preloaderPlayed')) {
-    window.addEventListener('load', welcome)
-    localStorage.setItem('preloaderPlayed', true);
-}
-else {
-    refs.preloaderGifWrapper.style.display = 'none';
-}
+// if (!localStorage.getItem('preloaderPlayed')) {
+//     window.addEventListener('load', welcome)
+//     localStorage.setItem('preloaderPlayed', true);
+// }
+// else {
+//     refs.preloaderGifWrapper.style.display = 'none';
+// }
 
-function onSecondPage() {
-    localStorage.setItem('preloaderPlayed', true)
-    i += 1
-}
+// function onSecondPage() {
+//     localStorage.setItem('preloaderPlayed', true)
+//     i += 1
+// }
 
-function welcome() {
-    refs.preloaderGifImage.setAttribute('src', img.src)
-    refs.preloaderGifImage.classList.add('animate__fadeIn')
+// function welcome() {
+//     refs.preloaderGifImage.setAttribute('src', img.src)
+//     refs.preloaderGifImage.classList.add('animate__fadeIn')
 
-    setTimeout(() => {
-        refs.preloaderGifWrapper.classList.add('animate__fadeOut')
-        refs.preloaderGifImage.classList.add('animate__fadeOut')
-        setTimeout(() => {
-            refs.preloaderGifWrapper.style.display = 'none';
-        }, 500)
-    }, 2000)
-}
+//     setTimeout(() => {
+//         refs.preloaderGifWrapper.classList.add('animate__fadeOut')
+//         refs.preloaderGifImage.classList.add('animate__fadeOut')
+//         setTimeout(() => {
+//             refs.preloaderGifWrapper.style.display = 'none';
+//         }, 500)
+//     }, 2000)
+// }
