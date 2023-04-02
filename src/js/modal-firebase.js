b123217d173c3edc1843cb282e88ea0fa794af0c
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-04-02 18:21:11 +0300

    fixed auth issue

--- src/js/modal-firebase.js
+++ src/js/modal-firebase.js
@@ -1,44 +1,29 @@
 import { refs } from './refs';
-console.log(document.location.host);
-
-if (
-  document.location.pathname === '/index.html' ||
-  (document.location.host && document.location.pathname !== '/my-library.html')
-) {
-  const initialModalHeaderText = refs.modalHeader.textContent;
-  const initialRegisterText = refs.registerText.textContent;
-  const initialRegisterButton = refs.loginButton.textContent;
-
-  refs.openModalBtn.addEventListener('click', openModal);
-  refs.closeModalBtn.addEventListener('click', closeModal);
-  refs.registerButton.addEventListener('click', register);
-
-  function openModal() {
-    refs.modal.style.display = 'block';
-  }
-
-  function closeModal() {
-    refs.modal.style.display = 'none';
-  }
+const initialModalHeaderText = refs.modalHeader.textContent;
+const initialRegisterText = refs.registerText.textContent;
+const initialRegisterButton = refs.loginButton.textContent;
+refs.openModalBtn.addEventListener('click', openModal);
+refs.closeModalBtn.addEventListener('click', closeModal);
+refs.registerButton.addEventListener('click', register);
+function openModal() {
+  refs.modal.style.display = 'block';
 }
-
+function closeModal() {
+  refs.modal.style.display = 'none';
+}
 window.onclick = function (event) {
   if (event.target === refs.modal) {
     closeModal();
   }
 };
-
 document.addEventListener('keydown', function (event) {
   if (event.key === 'Escape' || event.code === 27) {
     closeModal();
   }
 });
-
 function register(event) {
   event.preventDefault();
-
   const isRegistering = refs.modalHeader.textContent !== 'Register';
-
   refs.modalHeader.textContent = isRegistering
     ? 'Register'
     : initialModalHeaderText;
@@ -49,7 +34,6 @@ function register(event) {
   refs.loginButton.textContent = isRegistering
     ? 'Register'
     : initialRegisterButton;
-
   if (isRegistering) {
     registerRender();
   } else {
@@ -58,39 +42,30 @@ function register(event) {
   }
   clearInputFields(); // видаляємо вміст полів введення
 }
-
 function registerRender() {
-  refs.modalForm.insertAdjacentHTML(
-    'afterbegin',
-    '<input class="modal-firebase__form-input" type="text" placeholder="Name*" id="input-name" required minlength="4" maxlength="25">'
-  );
-
+  refs.modalForm.insertAdjacentHTML('afterbegin', '<input class="modal-firebase__form-input" type="text" placeholder="Name*" id="input-name" required minlength="4" maxlength="25">');
   const input = document.getElementById('input-name');
   input.addEventListener('input', () => {
     const isValid = input.checkValidity();
     input.style.borderBottom = isValid ? '2px solid green' : '2px solid red';
     input.style.color = isValid ? 'green' : 'red';
   });
-
   input.addEventListener('blur', () => {
     if (input.value === '') {
       input.style.borderBottom = '2px solid red';
     }
   });
 }
-
 //видаляє символи в інпути коли натискаєш на кнопку
 function clearInputFields() {
   refs.inputFields.forEach(input => (input.value = ''));
 }
-
 // Функція перевірки поля введення та оновлення його стилів
 function validateInputField(inputField) {
   const isValid = inputField.checkValidity();
   inputField.style.borderBottom = isValid ? '2px solid green' : '2px solid red';
   inputField.style.color = isValid ? 'green' : 'red';
 }
-
 // Функція для додавання слухачів подій для перевірки введення до кожного поля введення
 function addInputValidationListeners(inputFields) {
   inputFields.forEach(inputField => {
@@ -111,7 +86,6 @@ function clearInputFieldStyles(inputFields) {
     inputField.style.color = '';
   });
 }
-
 // Основна функція для налаштування перевірки поля введення та очищення стилів після натискання кнопки реєстрації
 function setInputFieldValidation({ inputFields, registerButton } = refs) {
   addInputValidationListeners(inputFields);
@@ -119,5 +93,4 @@ function setInputFieldValidation({ inputFields, registerButton } = refs) {
     clearInputFieldStyles(inputFields);
   });
 }
-
-setInputFieldValidation();
+setInputFieldValidation()
