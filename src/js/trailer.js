bc6154e8d5df856e7ed85d06fae7e0fcc707e719
Swarp <m.kleban@terawatt-group.com>
2023-04-03 15:30:36 +0300

    частково пофіксив відео

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -9,25 +9,26 @@ refs.backdropTrailer.addEventListener("click", switcherTrailerModal);
 
 async function switcherTrailerModal() {
      let data = await GetTrailer(filmId);
-     console.log(data);
-     const getLink = data.results[0].key;
-     const markup =
-       await `<iframe class="youtube" width="100%" height="100%" src='https://www.youtube.com/embed/${getLink}' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="trailer_video"></iframe>`;
+    console.log(data);
+    if (data.results.length>0){const getLink = data.results[0].key;
+    const markup =
+      await `<iframe class="youtube" width="100%" height="100%" src='https://www.youtube.com/embed/${getLink}' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="trailer_video"></iframe>`;
     let player = document.getElementById('video-trailer-player');
     player.innerHTML = markup;
-    refs.backdropTrailer.classList.toggle("is-hidden");
+    refs.backdropTrailer.classList.toggle('is-hidden');}
+     
 }
 
 
  //если не находит трейлер при открытии модалки, не отображается кнопка
 
- let data = GetTrailer(filmId);
- data.then(data => {
-  // const trailerNotExists = true; 
-  // - это для проверки, если не находит трейлер, не показывает кнопку
-  const trailerNotExists = data.results.length === 0;
-  if (trailerNotExists) {
-      refs.trailerBtn.classList.toggle('is-hidden');
-  }
- });
+//  let data = GetTrailer(filmId);
+//  data.then(data => {
+//   // const trailerNotExists = true; 
+//   // - это для проверки, если не находит трейлер, не показывает кнопку
+//   const trailerNotExists = data.results.length === 0;
+//   if (trailerNotExists) {
+//       refs.trailerBtn.classList.toggle('is-hidden');
+//   }
+//  });
 
