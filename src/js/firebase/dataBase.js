e69ed8be79ec92d2561257dc76a64204ce586560
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-04-03 08:47:50 +0300

    new

--- src/js/firebase/dataBase.js
+++ src/js/firebase/dataBase.js
@@ -1,62 +1,69 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { getDatabase, ref, set, child, get } from 'firebase/database';
 
-// const buttonWatched = document.querySelector('.library__button.watched');
-// const container = document.querySelector('.library-wrap');
-// const modalFilm = document.querySelector('.backdrop-film-card');
-// console.dir(modalFilm);
-
-// const filmWatched = createMerkaup('filmWatched');
-
-// buttonWatched.addEventListener('click', () => {
-//   container.insertAdjacentHTML('afterbegin', filmWatched);
-// });
-
 const auth = getAuth();
 const db = getDatabase();
 const dbRef = ref(getDatabase());
+const filmsArray = [];
 
-const filmWatchedArr = [
-  {
-    username: 'Admin',
-    email: 'email',
-    profile_picture: 'imageUrl',
-  },
-  {
-    username: 'Ada',
-    email: 'email',
-    profile_picture: 'imageUrl',
-  },
-];
+const modal = document.querySelector('.modal-film__main');
 
-// Поменя разметку когда будут фильмы
+modal.addEventListener('click', createFilmObj);
 
-function createMerkaup(storage) {
-  return JSON.parse(localStorage.getItem(storage))
-    .map(film => {
-      return `<div>${film.username}</div>`;
-    })
-    .join('');
+function createFilmObj(e) {
+  const targetFilm = e.target.dataset;
+  if (e.target.textContent === 'add to watched') {
+    console.dir(e.target);
+
+    filmsArray.push({
+      id: targetFilm.id,
+      title: targetFilm.title,
+      genre_name: targetFilm.genre_name,
+      poster_path: targetFilm.poster_path,
+      original_title: targetFilm.original_title,
+      vote_average: targetFilm.vote_average,
+      popularity: targetFilm.popularity,
+      vote_count: targetFilm.vote_count,
+      genres: targetFilm.genres,
+      overview: targetFilm.overview,
+    });
+  }
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      set(ref(db, 'users/' + 'ovrGn2FJIdTUQrajvyrFQ3Gb5bs1/'), filmsArray);
+    }
+  });
+
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      get(child(dbRef, `users/ovrGn2FJIdTUQrajvyrFQ3Gb5bs1`))
+        .then(snapshot => {
+          if (snapshot.exists()) {
+            localStorage.setItem('filmWatched', JSON.stringify(snapshot.val()));
+          } else {
+            console.log('No data available');
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  });
 }
 
-onAuthStateChanged(auth, user => {
-  if (user) {
-    set(ref(db, 'users/' + 'ovrGn2FJIdTUQrajvyrFQ3Gb5bs1/'), filmWatchedArr);
-  }
-});
+// if (
+//   document.location.pathname !== '/index.html' ||
+//   (document.location.host && document.location.pathname === '/my-library.html')
+// ) {
+//   const btnWatched = document.querySelector('.library__button.watched');
+//   console.log(btnWatched);
+// }
+// Поменя разметку когда будут фильмы
 
-onAuthStateChanged(auth, user => {
-  if (user) {
-    get(child(dbRef, `users/ovrGn2FJIdTUQrajvyrFQ3Gb5bs1`))
-      .then(snapshot => {
-        if (snapshot.exists()) {
-          localStorage.setItem('filmWatched', JSON.stringify(snapshot.val()));
-        } else {
-          console.log('No data available');
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-});
+// function createMerkaup(storage) {
+//   return JSON.parse(localStorage.getItem(storage))
+//     .map(film => {
+//       return `<div>${film.username}</div>`;
+//     })
+//     .join('');
+// }
