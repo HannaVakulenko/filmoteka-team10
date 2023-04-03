1a72ab4dbe9ed8a8bb43b157339e83f70acf7ba5
Skripnuyk <walker8613@gmail.com>
2023-04-03 16:30:35 +0300

    Переробив pagination.js
    Додав кнопки пагінаціі

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -2,49 +2,45 @@ import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 import { refs } from './refs';
 
-const TUI_VISIBLE_PAGES = 5;
+const options = {
 
-export function createPagination(totalItems, visiblePages) {
-    const options = {
-        itemsPerPage: 12,
-        totalItems: totalItems,
-        visiblePages: visiblePages < 5 ? visiblePages : TUI_VISIBLE_PAGES,
-        centerAlign: true,
-    };
+  itemsPerPage: 1,
+  visiblePages: 5,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
 
-    const pagination = new Pagination(refs.pagination, options);
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
 
-    if (visiblePages > 1) {
-    refs.pagination.style.display = 'block';
+const pagination = new Pagination(refs.pagination, options);
+
+export function createPagination(totalPages) {
+  if (totalPages <= 1) {
+    refs.divPagination.classList.add('hidden-tui');
+  } else {
+    refs.divPagination.classList.remove('hidden-tui');
+    if (totalPages !== pagination._options.totalItems) {
+      pagination.reset(totalPages);
     } else {
-        refs.pagination.style.display = 'none';
+      pagination.setTotalItems(totalPages);
     }
-
-    return pagination;
+  }
 }
 
-// const pagination = createPagination(data.total_results, data.total_pages); для того щоб запустити пагінацію
+export { pagination, options };
 
-
-// pagination.on('beforeMove', ({ page }) => {
-//         spinnerStart();
-//         refs.galleryFilms.innerHTML = '';
-//         FetchSearch(query, page).then(data => {
-//             spinnerEnd();
-//             refs.galleryFilms.innerHTML =  renderGallery(data.results);
-//             scrollOnTop();
-//         });
-//         });
-
-
-
-// pagination.on('beforeMove', ({ page }) => {
-//         spinnerStart();
-//         refs.galleryFilms.innerHTML = '';
-//         FetchTrending(query, page).then(data => {
-//             spinnerEnd();
-//             refs.galleryFilms.innerHTML = renderGallery(data.results);
-//             scrollOnTop();
-//         });
-//         });
-    
